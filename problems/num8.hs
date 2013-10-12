3a5f88a45197376894e8282ea9ffb4d469cf476e
dveim <alexandrolgash@gmail.com>
2013-10-12 17:36:12 +0300

    simple graph

---
+++ problems/num8.hs
@@ -0,0 +1,4 @@
+import Math.Combinatorics.Graph --didn't i say about reinventing the wheel?
+
+--further information on http://hackage.haskell.org/package/HaskellForMaths-0.4.5/docs/Math-Combinatorics-Graph.html
+--example of usage: findPaths (c 4) 1 2
